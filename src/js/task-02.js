08cf80b896da896892c866d3ec3d6fa1d25a5b32
Viktoriya Shpachynska <viktoriya.shpachynska@gmail.com>
2021-07-01 09:12:18 +0300

    adds task-2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,22 @@
+const listEl = document.querySelector("#ingredients");
+console.log(listEl);
+
 const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
 ];
+
+const createListItems = (ingredients) =>
+  ingredients.map((ingredient) => {
+    const listItemEl = document.createElement("li");
+    listItemEl.textContent = ingredient;
+    return listItemEl;
+  });
+
+const ingredientList = createListItems(ingredients);
+
+listEl.append(...ingredientList);
